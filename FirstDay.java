30db1459035fec70ca4df73dce75895b70c24626
ysx <3597107837@qq.com>
2020-01-14 11:54:58 +0800

    作业

--- FirstDay.java
+++ FirstDay.java
@@ -7,6 +7,8 @@ import java.util.Random;
 
 /**
  * @author ysx
+ * 2020.1.13
+ * 随机产生100个0到99之间的整数，并且输出到文件中
  */
 public class FirstDay {
     public static void main(String[] args) throws IOException {
@@ -23,8 +25,8 @@ public class FirstDay {
 
         //存储位置
         File file = new File("E:\\array.txt");
-        //文件输入流
-        FileWriter out = new FileWriter(file);
+        //文件输入流,再次运行文件后原有文件不会被覆盖
+        FileWriter out = new FileWriter(file,true);
         //将数组中的元素写入到文件中
         for (int i=0;i<100;i++){
             out.write(array[i] + "\t");
